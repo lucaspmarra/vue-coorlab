cb8e068770e2cb3eebd349bff928faf9f93d1a89
Lucas Marra <marralucas1@gmail.com>
2023-06-09 16:10:13 -0300

    update projeto

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -5,48 +5,78 @@
         <b class="ml-2"><b-icon icon="truck"></b-icon> {{ appName }}</b>
       </b-navbar-brand>
     </b-navbar>
-    <ContentBox :data="transportData"/>
+    <!-- <ContentBox :data="options"/> -->
+    <b-form-select v-model="selectedCity">
+      <option v-for="city in uniqueCities" :key="city" :value="city">
+        {{ city }}
+      </option>
+    </b-form-select>
+    <div v-if="selectedCity">
+      <h2>Cidade selecionada: {{ selectedCity }}</h2>
+      <div v-for="option in filteredOptions" :key="option.id">
+        <h3>{{ option.name }}</h3>
+        <p>Custo de Frete até 100Kg [R$/Kg]: {{ option.cost_transport_light }}</p>
+        <p>Custo de Frete mais de 100Kg [R$/Kg]: {{ option.cost_transport_heavy }}</p>
+        <p>Cidade de destino: {{ option.city }}</p>
+        <p>Tempo de entrega: {{ option.lead_time }}</p>
+      </div>
+    </div>
   </div>
-
 </template>
 
 <script>
-import {
-  BNavbar,
-  BNavbarBrand,
-} from 'bootstrap-vue'
+import { BNavbar, BNavbarBrand } from 'bootstrap-vue'
 import axios from 'axios'
-import ContentBox from './ContentBox.vue'
+// import ContentBox from './ContentBox.vue'
 
 export default {
   components: {
     BNavbar,
     BNavbarBrand,
-    ContentBox,
+    // ContentBox,
   },
-  data() {
-    const appName = ''
-
+  data () {
     return {
-      appName,
-      transportData: null,
+      appName: 'Melhor Frete',
+      options: [],
+      selectedCity: null,
     }
   },
-  created() {
-    // Implemente aqui o GET dos dados da API REST
-    // para que isso ocorra na inicialização da pagina
-    this.getAPI()
-    this.appName = 'Melhor Frete'
-  },
   methods: {
-    // Implemente aqui os metodos utilizados na pagina
-    methodFoo() {
-      console.log(this.appName)
+    async getAPI () {
+      try {
+        const response = await axios.get('http://localhost:3000/transport')
+        this.options = response.data;
+      } catch (error) {
+        console.error(error)
+      }
     },
-    getAPI(){
-      axios.get('http://localhost:3000/transport').then(response => 
-      this.transportData = response.data).catch(error => console.error(error))
+    async fetchTransportData (city) {
+      try {
+        const response = await axios.get('http://localhost:3000/transport');
+        const filteredOptions = response.data.filter((option) => option.city === city);
+        console.log(`Dados de transporte para: ${city}`, filteredOptions);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+  computed: {
+    uniqueCities () {
+      return [...new Set(this.options.map((option) => option.city))];
     },
+    filteredOptions () {
+      return this.selectedCity ? this.options.filter((option) => option.city === this.selectedCity) : [];
+    },
+  },
+  watch: {
+    selectedCity (newCity) {
+      console.log(`Cidade selecionada: ${newCity}`);
+      this.fetchTransportData(newCity);
+    },
+  },
+  created () {
+    this.getAPI()
   },
 }
 </script>
@@ -58,15 +88,16 @@ export default {
 }
 
 
-.title .navbar-brand, b {
+.title .navbar-brand,
+b {
   margin-left: 20px;
-  
+
 }
 
 .container {
   background: #00aca6;
   padding: 2rem;
   margin-top: 2rem;
-  box-shadow: 0px 0px 5px 3px rgba(0,0,0,0.75);
+  box-shadow: 0px 0px 5px 3px rgba(0, 0, 0, 0.75);
 }
 </style>
--- code/src/components/ContentBox.vue
+++ code/src/components/ContentBox.vue
@@ -1,51 +1,66 @@
 <template>
   <div class="content">
     <div class="form">
-      <b-form>
+      <b-form @submit.prevent="submitForm">
         <h1><i></i>Insira o destino e o peso</h1>
         <b-form-group class="w-100" id="destino" label="Destino">
-          <b-form-select class="w-100" v-model="selected" :options="data">
-          </b-form-select>
+
+          <b-form-select v-model="selected" :options="data"></b-form-select>
+
         </b-form-group>
+
         <b-form-group id="peso" label="Peso">
-          <b-form-input class="md-6" placeholder="Peso da carga em kg"></b-form-input>
+          <b-form-input v-model="weight" class="md-6" placeholder="Peso da carga em kg"></b-form-input>
         </b-form-group>
-      <b-form-group><b-button class="botao" type="submit" variant="primary">Analisar</b-button></b-form-group>
+
+        <b-form-group>
+          <b-button class="botao" type="submit" variant="primary">Analisar</b-button>
+        </b-form-group>
       </b-form>
     </div>
-    <div class="result">result</div>
+    <div class="result">
+      <h2>Resultado:</h2>
+      <pre>{{ result }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
 
-    props:{
-      data:{
-        type: Array,
-        required: true,
-      }
-    },
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    }
+  },
 
-    data(){
-      return{
-        selected: "null",
-        options: [
-          { value: null, text: 'Selecione o destino', disabled: true}
-
-        ]
+  data () {
+    return {
+      selected: null,
+      weight: null,
+      result: null,
+    };
+  },
+  computed: {},
+  methods: {
+    submitForm () {
+      if (!this.selected || !this.weight) {
+        alert('Por favor, selecione o destino e insira o peso.');
+        return;
       }
     }
-  }
+  },
+}
 </script>
 
 <style scoped>
-
-.content{
+.content {
   width: auto;
   display: flex;
   justify-content: space-between;
 }
+
 .form {
   width: 45%;
   height: 50vh;
@@ -54,9 +69,9 @@
   justify-content: center;
   padding: 2rem;
 }
-.botao{
+
+.botao {
   margin-top: 2rem;
   justify-content: center;
 }
-
 </style>
